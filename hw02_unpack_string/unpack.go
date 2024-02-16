541ba2a6149879afeff6a36ec08400c553c7d30b
anton.goloschapov <anton.goloschapov@rtlabs.ru>
2024-02-16 08:57:30 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,43 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	builder := strings.Builder{}
+	var err error = nil
+	var idx = 0
+	for i, v := range str {
+		//Проверяем является ли элемент числом
+		count, e := strconv.Atoi(string(str[i]))
+		if e == nil && i == 0 { // Число стоит первым? Ошибка!
+			err = ErrInvalidString
+			break
+		} else if e == nil && i > 0 { //Число не стоит первым? ок
+			_, e2 := strconv.Atoi(string(str[i-1])) //А что стоит перед ним?
+			if e == nil && e2 == nil {              // Два числа подряд? Ошибка!
+				err = ErrInvalidString
+				break
+			}
+		}
+		if e == nil {
+			if count != 0 {
+				builder.WriteString(strings.Repeat(string(str[i-1]), count-1))
+			}
+			if count == 0 {
+				idx++ // Фиксируем отклонение индекса
+				result := builder.String()
+				result = result[:i-idx]
+				builder.Reset()
+				builder.WriteString(result)
+			}
+		} else {
+			builder.WriteString(string(v))
+		}
+	}
+	return builder.String(), err
 }
