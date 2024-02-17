62380bc7cd8523abc9e659f72b831ebdd2474b68
anton.goloschapov <lanworm@mail.ru>
2024-02-17 14:35:18 +0300

    linter

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,85 +9,15 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (string, error) {
-
 	builder := strings.Builder{}
 
 	var err error
 
-	var idx = 0
-
-	for i, v := range str {
-
-		count, e := strconv.Atoi(string(str[i]))
-
-		if e == nil && i == 0 {
-
-			err = ErrInvalidString
-
-			break
-
-		} else if e == nil && i > 0 {
-
-			_, e2 := strconv.Atoi(string(str[i-1]))
-
-			if e == nil && e2 == nil {
-
-				err = ErrInvalidString
-
-				break
-
-			}
-
-		}
-
-		if e == nil {
-
-			if count != 0 {
-
-				builder.WriteString(strings.Repeat(string(str[i-1]), count-1))
-
-			}
-
-			if count == 0 {
-
-				idx++
-
-				result := builder.String()
-
-				result = result[:i-idx]
-
-				builder.Reset()
-
-				builder.WriteString(result)
-
-			}
-
-		} else {
-
-			builder.WriteString(string(v))
-
-		}
-
-	}
-
-	return builder.String(), err
-
-}
-
-import (
-	"errors"
-	"strconv"
-	"strings"
-)
-
-var ErrInvalidString = errors.New("invalid string")
-
-func Unpack(str string) (string, error) {
-	builder := strings.Builder{}
-	var err error
 	idx := 0
+
 	for i, v := range str {
 		count, e := strconv.Atoi(string(str[i]))
+
 		if e == nil && i == 0 {
 			err = ErrInvalidString
 			break
@@ -98,6 +28,7 @@ func Unpack(str string) (string, error) {
 				break
 			}
 		}
+
 		if e == nil {
 			if count != 0 {
 				builder.WriteString(strings.Repeat(string(str[i-1]), count-1))
@@ -113,5 +44,6 @@ func Unpack(str string) (string, error) {
 			builder.WriteString(string(v))
 		}
 	}
+
 	return builder.String(), err
 }
