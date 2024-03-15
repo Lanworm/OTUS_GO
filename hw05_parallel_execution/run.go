6d7e012b818cd869978dee4bbf607026558a25ae
anton.goloschapov <anton.goloschapov@rtlabs.ru>
2024-03-15 11:41:40 +0300

    fix v3

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -11,17 +11,14 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 type Task func() error
 
 func worker(ch chan Task, errorCount *int64, wg *sync.WaitGroup) {
-	for {
-		t, ok := <-ch
-		if !ok {
-			break
-		}
+	wg.Add(1)
+	for t := range ch {
 		err := t()
 		if err != nil {
 			atomic.AddInt64(errorCount, 1)
 		}
-		wg.Done()
 	}
+	wg.Done()
 }
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
@@ -30,15 +27,16 @@ func Run(tasks []Task, n, m int) error {
 	var result error
 	var errorCount int64
 	tasksCh := make(chan Task)
-	defer close(tasksCh)
+	//defer close(tasksCh)
 	for i := 0; i < n; i++ {
 		go worker(tasksCh, &errorCount, &wg)
 	}
 	for _, t := range tasks {
 		if int(errorCount) != 0 && int(errorCount) == m {
 			result = ErrErrorsLimitExceeded
+			close(tasksCh)
+			break
 		}
-		wg.Add(1)
 		tasksCh <- t
 	}
 	wg.Wait()
