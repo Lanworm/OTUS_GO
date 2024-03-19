2abf090fdacc98c5dfa4f1fa065051e83505fcf7
anton.goloschapov <anton.goloschapov@rtlabs.ru>
2024-03-19 15:47:41 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,48 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	outArr := make([]Out, 0)
+	outFlagArr := make([]bool, 0)
+	resultArr := make([]interface{}, 0)
+	for task := range in {
+		out := make(Bi, 1)
+		out <- task
+		in = out
+		for _, s := range stages {
+			in = s(in)
+		}
+		outArr = append(outArr, in)
+		outFlagArr = append(outFlagArr, false)
+		resultArr = append(resultArr, nil)
+	}
+	result := make(Bi, len(resultArr))
+	defer close(result)
+	for {
+		select {
+		case _, ok := <-done:
+			if !ok {
+				return result
+			}
+		default:
+		}
+		for i := 0; i < len(outArr); i++ {
+			select {
+			case x := <-outArr[i]:
+				outFlagArr[i] = true
+				resultArr[i] = x
+			default:
+			}
+		}
+		quit := true
+		for _, f := range outFlagArr {
+			quit = quit && f
+		}
+		if quit {
+			break
+		}
+	}
+	for _, v := range resultArr {
+		result <- v
+	}
+	return result
 }
