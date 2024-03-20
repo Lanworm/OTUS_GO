3b014c27125876240f4f618da3eaf7acfcbe9081
anton.goloschapov <anton.goloschapov@rtlabs.ru>
2024-03-20 17:13:42 +0300

    fix v1

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,49 +8,25 @@ type (
 
 type Stage func(in In) (out Out)
 
-func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	outArr := make([]Out, 0)
-	outFlagArr := make([]bool, 0)
-	resultArr := make([]interface{}, 0)
-	for task := range in {
-		out := make(Bi, 1)
-		out <- task
-		in = out
-		for _, s := range stages {
-			in = s(in)
-		}
-		outArr = append(outArr, in)
-		outFlagArr = append(outFlagArr, false)
-		resultArr = append(resultArr, nil)
-	}
-	result := make(Bi, len(resultArr))
-	defer close(result)
-	for {
-		select {
-		case _, ok := <-done:
-			if !ok {
-				return result
-			}
-		default:
-		}
-		for i := 0; i < len(outArr); i++ {
+func exec(done In, intStream In, stage Stage) {
+	resultStream := make(Bi)
+	go func() {
+		defer close(resultStream)
+		for range intStream {
 			select {
-			case x := <-outArr[i]:
-				outFlagArr[i] = true
-				resultArr[i] = x
-			default:
+			case <-done:
+				return
+			case resultStream <- stage(intStream):
 			}
 		}
-		quit := true
-		for _, f := range outFlagArr {
-			quit = quit && f
-		}
-		if quit {
-			break
-		}
+	}()
+	intStream = resultStream
+}
+
+func ExecutePipeline(in In, done In, stages ...Stage) Out {
+	out := stages[0](in)
+	for _, s := range stages {
+		exec(done, out, s)
 	}
-	for _, v := range resultArr {
-		result <- v
-	}
-	return result
+	return out
 }
