a37b1f0e13ef790ff05a98b939e00c6ca566772f
anton.goloschapov <anton.goloschapov@rtlabs.ru>
2024-03-23 08:54:12 +0300

    fix v4

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,24 +8,27 @@ type (
 
 type Stage func(in In) (out Out)
 
-func exec(done In, in In, out Out) {
+func exec(in In, done In) Out {
+	out := make(Bi)
 	go func() {
 		for {
 			select {
 			case <-in:
-				in = out
+				v := <-in
+				out <- v
 			case <-done:
 				return
 			}
 		}
 	}()
+	return out
 }
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	out := make(Out)
+	out := make(In)
 	for _, s := range stages {
-		out = s(in)
-		exec(done, out, in)
+		out = exec(in, done)
+		in = s(out)
 	}
 	return out
 }
